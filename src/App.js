068ccb9261330be08fb31fb3856cac57511731a5
Felipe Miguel dos Santos <felipe.miguel@csptech.com.br>
2023-07-02 13:50:32 -0300

    ST-29

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -32,6 +32,22 @@ const App = () => {
   setInterval(() => {
     document.querySelector('iframe')?.remove()
   }, 1000)
+
+  const setZoom = () => {
+    const zoomLevel = 0.9 // Defina o nível de zoom desejado (90% = 0.9)
+    document.body.style.zoom = zoomLevel
+  }
+
+  useEffect(() => {
+    setZoom()
+    // Para atualizar o zoom se a janela do navegador for redimensionada
+    window.addEventListener('resize', setZoom)
+
+    return () => {
+      // Remova o ouvinte do evento quando o componente for desmontado
+      window.removeEventListener('resize', setZoom)
+    }
+  }, [])
 
   return (
     <React.Fragment>
